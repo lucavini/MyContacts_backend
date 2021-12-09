38a978e65963d82a0ddaaca65c844ca80d290cb0
lucavini <lucass.lv.lv@gmail.com>
2021-12-09 13:30:16 -0300

    conexao com o banco de dados

--- BackEnd/src/app/repositories/ContactRepository.js
+++ BackEnd/src/app/repositories/ContactRepository.js
@@ -1,5 +1,7 @@
 const { v4 } = require('uuid');
 
+const db = require('../../database');
+
 let contacts = [
   {
     id: v4(),
@@ -18,35 +20,35 @@ let contacts = [
 ];
 
 class ContactRepository {
-  findAll() {
-    return new Promise((resolve) => resolve(contacts));
+  async findAll() {
+    const rows = await db.query('SELECT * FROM contacts');
+
+    return rows;
   }
 
-  findById(id) {
-    return new Promise((resolve) =>
-      resolve(contacts.find((contact) => contact.id === id))
+  async findById(id) {
+    const [row] = await db.query('SELECT * FROM contacts WHERE id = $1', [id]);
+    return row;
+  }
+
+  async findByEmail(email) {
+    const row = await db.query('SELECT * FROM contacts WHERE email = $1', [
+      email,
+    ]);
+    return row;
+  }
+
+  async create({ name, email, phone, category_id }) {
+    const row = await db.query(
+      `
+      INSERT INTO contacts(name, email, phone, category_id) 
+      VALUES($1, $2, $3, $4)
+      RETURNING *
+      `,
+      [name, email, phone, category_id]
     );
-  }
 
-  findByEmail(email) {
-    return new Promise((resolve) => {
-      resolve(contacts.find((contact) => contact.email === email));
-    });
-  }
-
-  create({ name, email, phone, category_id }) {
-    return new Promise((resolve) => {
-      const newContact = {
-        id: v4(),
-        name,
-        email,
-        phone,
-        category_id,
-      };
-
-      contacts.push(newContact);
-      resolve(newContact);
-    });
+    return row[0];
   }
 
   update(id, { name, email, phone, category_id }) {
